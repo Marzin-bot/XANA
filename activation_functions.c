785babc6718838c87989b3ddb65ee6ecd9c326a5
Marzin <70151472+Marzin-bot@users.noreply.github.com>
2021-08-07 23:49:21 +0200

    Create activation_functions.c

---
+++ activation_functions.c
@@ -0,0 +1,31 @@
+#include <math.h>
+#include "activation_functions.h"
+/////////////// FONCTIONS D'ACTIVATION AVEC LEURS DÉRIVÉ RESPECTIF //////////////
+/////Fonctions d'activation
+float sigmoid(float s){
+	return 1 / (1 + exp(-s));
+}
+
+//dériver de la sigmoide
+float sigmoidPrime(float s){
+	return s * (1 - s);
+}
+
+
+float ReLU(float x){
+	return fmaxf(x, 0);
+}
+
+
+float ReLUPrime(float x){
+	if (x > 0){
+		return 1;
+	}
+	
+	return 0;
+}
+
+
+float swish(float x, float b){
+	return x * sigmoid(b * x);
+}
